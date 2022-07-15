de944f6ecf56a1d4481cf64593771955ce7a12dd
SazidAhmed <sazidahmed.official@gmail.com>
2022-07-16 04:14:49 +0600

    account info display completed

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,8 +1,45 @@
-function Account() {
+import { useState, useEffect } from 'react';
 
-    return (
+function Account() {
+  const [account, setAccount] = useState(null);
+
+  //Hook
+  useEffect(()=>{
+    getAccountInfo()
+  }, []);
+
+  const getAccountInfo = ()=>{
+    const localData = localStorage.getItem('wallet');
+    console.log(localData)
+    if(localData){
+      setAccount(JSON.parse(localData));
+    }
+  }
+
+  return (
     <>
-        <h1>Account</h1>
+      <div className="container">
+        {account &&
+          <div className="row">
+            <div className="alert alert-info alert-dismissible fade show text-break mt-4" role="alert">
+              <div className="row">
+                <div className="col">
+                <strong>Nid : </strong>{account.nid}
+                </div>
+                <div className="col">
+                <strong>Vote : </strong>{account.vote}
+                </div>
+              </div>
+            </div>
+            <div className="alert alert-success alert-dismissible fade text-break show mt-2" role="alert">
+              <strong>Public Key : </strong>{account.public_key}
+            </div>
+            <div className="alert alert-danger alert-dismissible fade text-break show mt-2" role="alert">
+              <strong>Private Key : </strong>{account.private_key}
+            </div>
+          </div>
+        }
+      </div>
     </>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios'
 
 function Login() {
 
@@ -26,6 +27,22 @@ function Login() {
         const publicKey = keyPair.getPublic('hex')
         console.log(publicKey);
         console.log(priKey);
+
+        const response = await axios.get('http://127.0.0.1:8000/api/voterGetByPubKey/'+publicKey);
+        if(response.data.query_status === '404'){
+            console.log(response.data.message);
+            setMessage(response.data.message);
+        }else if(response.data.query_status === '200'){
+            console.log(response.data.message);
+            setMessage(response.data.message);
+            const walletData = {
+                'nid':response.data.nid, 
+                'public_key': publicKey,
+                'private_key': priKey,
+                'vote': response.data.vote
+            }
+            localStorage.setItem('wallet', JSON.stringify(walletData));
+        }
         
         setPriKey('');
     }
