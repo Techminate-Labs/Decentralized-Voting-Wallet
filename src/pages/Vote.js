ac53030883e5bb051712b4deb34f084e92ee602b
SazidAhmed <sazidahmed.official@gmail.com>
2022-07-18 21:54:42 +0600

    accouts added

--- src/pages/Vote.js
+++ src/pages/Vote.js
@@ -1,8 +1,23 @@
 function Vote() {
 
-    return (
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+  }
+
+  return (
     <>
-        <h1>Vote</h1>
+      <div className="container">
+        <div className="row">
+            <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+                <label htmlFor="priKey" className="form-label">Your Account Private Key : </label>
+                <input type="text" className="form-control" id="priKey" name='priKey' placeholder="Enter Your Private Key Here" aria-describedby="emailHelp" />
+            </div>
+            <button type="submit" className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+      </div>
     </>
   );
 }
